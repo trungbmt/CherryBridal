8c17e303f01833927d9af206888aa701204e0590
phamtrantriem <74364380+phamtrantriem@users.noreply.github.com>
2020-12-30 20:14:06 +0700

    fe

--- resources/views/user/product-details.blade.php
+++ resources/views/user/product-details.blade.php
@@ -214,7 +214,7 @@
             <div class="col-12 col-md-6">
                 <div class="single_product_desc">
 
-                    <h4 class="title"><a href="#">{{$product->product_name}}</a></h4>
+                    <h4 class="title"><a href="#"><h2>{{$product->product_name}}</h2></a></h4>
 
                     <h4 class="price">{{$product->get_lowest_price()}}</h4>
 
@@ -246,14 +246,27 @@
                             <input type="number" class="qty-text" id="qty" step="1" min="1" max="12" name="quantity" value="1">
                             <span class="qty-plus" onclick="var effect = document.getElementById('qty'); var qty = effect.value; if( !isNaN( qty )) effect.value++;return false;"><i class="fa fa-plus" aria-hidden="true"></i></span>
                         </div>
-                        <button onclick="add_to_cart()" value="5" class="btn cart-submit d-block">Add to cart</button>
+                        <button onclick="add_to_cart()" value="5" style="width: 40%" class="btn cart-submit d-block">Thêm vào giỏ hàng</button>
                     </div>
 
                     <div id="accordion" role="tablist">
                         <div class="card">
+                            <div class="card-header" role="tab" id="headingTwo">
+                                <h6 class="mb-0">
+                                    <a class="collapsed" data-toggle="collapse" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">Thông tin chi tiết</a>
+                                </h6>
+                            </div>
+                            <div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo" data-parent="#accordion">
+                                <div class="card-body">
+                                    <p>Chất liệu: abc</p>
+                                    <p>Kích thước: 000mm x 00mm X 00mm</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
                             <div class="card-header" role="tab" id="headingOne">
                                 <h6 class="mb-0">
-                                    <a data-toggle="collapse" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Information</a>
+                                    <a data-toggle="collapse" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Mô tả</a>
                                 </h6>
                             </div>
 
@@ -263,20 +276,8 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="card">
-                            <div class="card-header" role="tab" id="headingTwo">
-                                <h6 class="mb-0">
-                                    <a class="collapsed" data-toggle="collapse" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">Cart Details</a>
-                                </h6>
-                            </div>
-                            <div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo" data-parent="#accordion">
-                                <div class="card-body">
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo quis in veritatis officia inventore, tempore provident dignissimos nemo, nulla quaerat. Quibusdam non, eos, voluptatem reprehenderit hic nam! Laboriosam, sapiente! Praesentium.</p>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia magnam laborum eaque.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card">
+                        
+                        {{-- <div class="card">
                             <div class="card-header" role="tab" id="headingThree">
                                 <h6 class="mb-0">
                                     <a class="collapsed" data-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">shipping &amp; Returns</a>
@@ -284,11 +285,10 @@
                             </div>
                             <div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#accordion">
                                 <div class="card-body">
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse quo sint repudiandae suscipit ab soluta delectus voluptate, vero vitae, tempore maxime rerum iste dolorem mollitia perferendis distinctio. Quibusdam laboriosam rerum distinctio. Repudiandae fugit odit, sequi id!</p>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae qui maxime consequatur laudantium temporibus ad et. A optio inventore deleniti ipsa.</p>
+                                    
                                 </div>
                             </div>
-                        </div>
+                        </div> --}}
                     </div>
 
                 </div>
@@ -324,7 +324,7 @@
                                     <a class="text-primary" href="#" onclick="
                                     $('#reply_view_{{$comment->id}}').css('display','block'); this.remove()
                                     ">
-                                    Xem {{$comment->replies()->count()}} câu trả lời
+                                    <br>Xem {{$comment->replies()->count()}} câu trả lời
                                     </a>
                                 @endif
                                 <div>
